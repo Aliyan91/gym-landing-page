02ff0b3cc85178d5ddae694b5e5679387397d2d0
Aliyan91 <aliyanamir184@gmail.com>
2025-08-08 12:47:08 +0500

    D

--- src/components/FAQ.svelte
+++ src/components/FAQ.svelte
@@ -1,5 +1,5 @@
 <script>
-  import Sectionwrapper from "./Sectionwrapper.svelte";
+  import SectionWrapper from "./SectionWrapper.svelte";
   let faqs = [
     {
       question: "What is Swoley Moley?",
@@ -44,7 +44,7 @@
   ];
 </script>
 
-<Sectionwrapper id="faqs">
+<SectionWrapper id="faqs">
   <div
     class="flex flex-col gap-10 sm:gap-14 md:gap-24 py-20 flex-1 items-center justify-center"
   >
@@ -77,4 +77,4 @@
       {/each}
     </div>
   </div>
-</Sectionwrapper>
+</SectionWrapper>
